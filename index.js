966d58730c2a549edffdbe4b218d7c99f75c3c71
Renat <jeingo20@gmail.com>
2022-12-09 18:34:47 +0300

    add end point

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('It works!')
 })
 
+app.get('/test', (req, res) => {
+    res.send('Test')
+})
+
+
 app.listen(process.env.PORT || 3000)
 
 module.exports = app
